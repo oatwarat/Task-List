cd5b48b6855830b74df2d323831c42d115255517
Warat Poovorakit <waratppp@gmail.com>
2024-03-16 23:58:34 +0700

    Initial Commit

---
+++ Task List/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Task List
+//
+//  Created by Warat Poovorakit on 16/3/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    
+
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    var items = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = " Task List"
+        view.addSubview(table)
+        table.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(didTapAdd))
+        
+    }
+    
+    @objc private func didTapAdd(){
+        let alert = UIAlertController(title: "New Item", message: "Enter new task list", preferredStyle: .alert)
+        
+        alert.addTextField {field in
+            field.placeholder = "Enter item..."
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { (_) in
+            if let field = alert.textFields?.first{
+                if let text = field.text, !text.isEmpty{
+                    
+                    DispatchQueue.main.async{
+                    self.items.append(text)
+                    self.table.reloadData()
+                    }
+                }
+            }
+        }))
+        
+        present(alert, animated: true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+
+}
+
